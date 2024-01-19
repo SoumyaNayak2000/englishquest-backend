cbc97f358e7017a30d1a1cbe2728f553188f3521
soumya nayak <soumyanayak.raju@gmail.com>
2024-01-19 16:24:19 +0530

    cors resolving

--- index.js
+++ index.js
@@ -12,21 +12,26 @@ import { BookRouter } from "./routes/book.js";
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "https://englishquest-frontend.vercel.app/",
+    credentials: true,
+  })
+);
 
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:9000");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-app.use(cors());
+// app.use(function (req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:9000");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   next();
+// });
 app.use(cookieParser());
 
 dotenv.config();
