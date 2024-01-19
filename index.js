dd68f9db2a099713bff0e66a0962eaf5eae5e444
soumya nayak <soumyanayak.raju@gmail.com>
2024-01-19 15:45:07 +0530

    credentials

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    // credentials: true,
+    credentials: true,
     origin: ["https://englishquest-frontend.vercel.app/"],
   })
 );
